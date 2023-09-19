05049eea299274bf049589f3d114c4b4074f3a03
Denneth Dacara <dennethdacara@gmail.com>
2023-09-20 00:40:07 +0800

    [hotfix] - append school name to html title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Academe Access</title>
+  <title>Academe Access - {{ $school->name }}</title>
   <link rel="icon" href="{{ $school->logo }}">
   <link rel="stylesheet" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
